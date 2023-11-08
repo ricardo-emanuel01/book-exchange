97144bdf942a7b25a5ae32ad01f4d7befd9d3c27
paularml <paula.rml@hotmail.com>
2023-11-08 19:10:29 -0300

    feat: base of the project

---
+++ src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+// app.use()
+
+
+app.listen(process.env.PORT)
+
+
