d1276744a0dfd795b92854ccd11aea361dd7692f
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2023-11-11 16:58:09 -0300

    Fix: Bug fixing in .env file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,7 +6,7 @@ const route = express();
 // rota de teste utilizando prisma
 route.get("/", async (req, res) => {
     try {
-        const users = await prisma.proposal.findMany() //trocar proposal por book ou user para testar
+        const users = await prisma.user.findMany() //trocar proposal por book ou user para testar
         res.status(200).json(users)
     } catch (error) {
         res.status(500).json("erro interno do servidor")
