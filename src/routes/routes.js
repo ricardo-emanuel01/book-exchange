4c9848130b1ad8454f63f77f18b3f2364120936d
paularml <paula.rml@hotmail.com>
2023-11-11 15:51:11 -0300

    feat: db test

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const prisma = require('../prisma/client');
+
+const route = express();
+
+// rota de teste utilizando prisma
+route.get("/", async (req, res) => {
+    try {
+        const users = await prisma.proposal.findMany() //trocar proposal por book ou user para testar
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json("erro interno do servidor")
+    }
+})
+
+module.exports = route
--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const route = require('./routes/routes')
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-// app.use()
+app.use(route)
 
 
 app.listen(process.env.PORT)
