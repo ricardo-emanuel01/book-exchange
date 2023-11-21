4e7e255c4b5e67318ed56e71238305a0f5559838
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-11-21 20:30:13 -0300

    refactor: catch

--- src/controllers/book/putBook.js
+++ src/controllers/book/putBook.js
@@ -23,11 +23,6 @@ const putBook = async (req, res) => {
 
     return res.status(200).json({ message: 'Livro atualizado com sucesso!' });
   } catch (error) {
-    console.log(error);
-    // Essa parte do código não chega a ser utilizada devido o intermediário que valida tokens
-    if (error.message === 'Token inválido') {
-      return res.status(401).json({ error: 'Não autorizado.' });
-    }
     if (error.code === 'P2025') return res.status(404).json({ message: 'Livro não encontrado.' });
 
     return res.status(500).json({ message: 'Erro interno do servidor.' });
